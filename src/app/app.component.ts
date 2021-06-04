4eb2f2cecff90d310a144af880ae6648195838d0
robin <RobinDmitriy@gmail.com>
2021-06-04 22:06:04 +0300

    4 урока Максима Грома по NGRX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {clear, countSelector, decrease, increase, updatedAtSelector} from './reducers/counter';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+
+
+  count$ = this.store.select(countSelector);
+  cannotDecrease$ = this.count$.pipe(map(count => count <= 0 ));
+  updatedAt$ = this.store.select(updatedAtSelector);
+
+  constructor(private store: Store) {
+  }
+
+  // get cannotDecrease(): boolean {
+  //   return false;
+  // }
+
+  increase(): void {
+    this.store.dispatch(increase());
+  }
+
+  decrease(): void {
+    this.store.dispatch(decrease());
+  }
+
+  clear(): void {
+    this.store.dispatch(clear());
+  }
 }
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,19 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector, createReducer,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import {counterReducer, CounterState, COUNTER_KEY} from './counter';
+
+export interface State {
+  [COUNTER_KEY]: CounterState;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  [COUNTER_KEY]: counterReducer,
+};
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
